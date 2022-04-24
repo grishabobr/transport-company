657eb0bde0f713819a41eef10c2b857512428cba
Григорий Жирнов <m2004350@edu.misis.ru>
2022-04-24 22:20:19 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,47 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div class="header">
+        <a class="logo" href="index.html">
+            <img src="assets/logo.svg" class="logoSvg"/>
+            <div class="logoTitle">
+                Транспортная компания
+            </div>
         </a>
-      </header>
+        <a class="menuItemCurrent" href="index.html">
+            Главная
+        </a>
+        <a class="menuItem" href="calculate.html">
+            Расчитать стоимость
+        </a>
+        <a class="menuItem" href="checkStatus.html">
+            Статус отправления
+        </a>
+        <a class="menuItem" href="contacts.html">
+            Контакты
+        </a>
+    </div>
+
+
+
+    <div class="content">
+        <div class="block">
+            <div class="blockText">
+                Оформляйте посылки онлайн по всей России
+            </div>
+            <a href="calculate.html" class="blockButtonLink">
+                <div class="blockButton">
+                    Отправить посылку
+                </div>
+            </a>
+        </div>
+    </div>
+
+
+    
+    <div class="footer">
+        © Все права защищены, 2022
+    </div>
     </div>
   );
 }
